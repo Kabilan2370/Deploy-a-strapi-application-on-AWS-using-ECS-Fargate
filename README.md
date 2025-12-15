217bf21aca1e135c23e095a1c9f51cd8fc6c3f29
Ubuntu <ubuntu@ip-172-31-11-13.eu-north-1.compute.internal>
2025-12-15 07:34:55 +0000

    updated

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# Deploy-a-strapi-application-on-AWS-using-ECS-Fargate
+# 🚀 Getting started with Strapi
+
+### strapi projects foler and files structure
+
+    my-strapi-app/
+    │
+    ├── Dockerfile
+    ├── .dockerignore
+    ├── package.json
+    ├── package-lock.json
+    ├── README.md
+    │
+    ├── config/                 
+    ├── src/                    # Strapi source files
+    ├── public/
+    │
+    ├── .github/
+    │   └── workflows/
+    │       ├── ci.yml          # CI: Build & push Docker image workflow
+    │       └── terraform.yml   # CD: Manual Terraform apply
+    │
+    └── terraform/              # All Terraform code
+        ├── main.tf
+        ├── variables.tf
+        ├── terraform.tfvars
+        ├── user_data.tpl       # EC2 startup script (install Docker + run Strapi)
+
+What Is the Task Concept?
+
+You are building a complete CI/CD automation system for deploying a Strapi application using:
+
+GitHub Actions (Automation)
+
+Docker (Build Strapi image)
+
+Terraform (Create AWS EC2 infrastructure)
+
+
+Default VPC (Networking)
+
+### Whenever you push code to GitHub, a new Strapi Docker image will be built → uploaded → and deployed to an EC2 server using Terraform.
+
+## Using .github/workflows create the resources by terraform --> do any changes on your repo  -->  When trigger the terraform.yml manually mentione the builded decker image tag then automatically pull that image from ECR then it will create a docker container.
+    
---
+++ terraform/main.tf
@@ -0,0 +1,91 @@
+
+# Get default VPC
+data "aws_vpcs" "default" {
+  filter {
+    name   = "isDefault"
+    values = ["true"]
+  }
+}
+
+locals {
+  default_vpc_id = data.aws_vpcs.default.ids[0]
+}
+
+# Get default subnets
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [local.default_vpc_id]
+  }
+}
+
+resource "aws_ecs_cluster" "cluster" {
+  name = "docker-strapi-cluster"
+}
+
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "docker-ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "ecs-tasks.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_exec_policy" {
+  role      = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# Security Group
+data "aws_security_groups" "strapi_sg" {
+  filter {
+    name   = "vpc-id"
+    values = [local.default_vpc_id]
+  }
+
+  filter {
+    name   = "group-name"
+    values = ["default"]
+  }
+}
+
+resource "aws_ecs_task_definition" "strapi" {
+  family                   		= "docker-strapi-task"
+  requires_compatibilities 	= ["FARGATE"]
+  network_mode             	= "awsvpc"
+  cpu                      		= 512
+  memory                   		= 1024
+  execution_role_arn       	= aws_iam_role.ecs_task_execution.arn
+
+  container_definitions = jsonencode([
+    {
+      name  		= "docker-strapi"
+      image 		= var.image_uri
+      portMappings = [
+        {
+          containerPort = 1337
+        }
+      ]
+      essential = true
+    }
+  ])
+}
+
+resource "aws_ecs_service" "strapi" {
+  name            	= "docker-strapi-service"
+  cluster         	= aws_ecs_cluster.cluster.id
+  task_definition 	= aws_ecs_task_definition.strapi.arn
+  desired_count   	= 1
+  launch_type     	= "FARGATE"
+
+  network_configuration {
+    subnet_id              = data.aws_subnets.default.ids[0]
+    vpc_security_group_ids = [data.aws_security_groups.strapi_sg.ids[0]]
+    assign_public_ip = true
+  }
+}
